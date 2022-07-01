fbf6666048519c85f9707194c0da5d6a683c4013
dlwnstjr1022 <dlwnstjr1022@gmail.com>
2022-07-02 03:10:33 +0800

    added remarks

--- 1929.py
+++ 1929.py
@@ -9,10 +9,10 @@ for i in range(M, N+1):
     if i == 1:
         continue
     
-    for j in range(2, int(i**0.5)+1):
+    for j in range(2, int(i**0.5)+1): # for else statment!!!
         if i%j == 0:
             break
-    else:
+    else: # else statement is executed when the for-loop is not interrupted (ie.break)
         print(i) 
 
 
