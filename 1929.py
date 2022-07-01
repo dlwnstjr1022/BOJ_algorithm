99edc57c47183c7895c73552bd4b5de92ae74aee
dlwnstjr1022 <dlwnstjr1022@gmail.com>
2022-07-02 00:50:48 +0800

    first commit

---
+++ 1929.py
@@ -0,0 +1,18 @@
+# BOJ 1929 소수구하기 
+# M 이상 N 이하의 소수 모두 출력 
+
+import sys 
+
+M, N = list(map(int,sys.stdin.readline().split()))
+
+for i in range(M, N+1):
+    if i == 1:
+        continue
+    
+    for j in range(2, int(i**0.5)+1):
+        if i%j == 0:
+            break
+    else:
+        print(i) 
+
+
